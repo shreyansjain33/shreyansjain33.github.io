e75766e61cd040d93a04f1ce49f035fcc033ab7d
Shreyans Kumar Jain <shreyansjain33@gmail.com>
2021-06-22 15:11:46 +0530

    Added Multiple Sections
    Initial Draft of the Home Page: Done.
    Multiple sections added along with FullPage.js and Splide Slider.

--- ionation/assets/js/components.js
+++ ionation/assets/js/components.js
@@ -8,3 +8,39 @@ function navMenu() {
     ? (menuBtn.classList.remove("active"), collapseNav.classList.remove("open"))
     : (menuBtn.classList.add("active"), collapseNav.classList.add("open"));
 }
+
+
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    autoScrolling:true,
+    paddingTop: '10vh',
+    paddingBottom: '5vh'
+  });
+});
+
+
+new Splide('#kb', {
+  type : 'loop',
+  fixedHeight: '40vh',
+  cover: true,
+  gap: '5vw',
+  focus: 'center',
+  padding: {
+    right: '30vw',
+    left : '30vw',
+  },
+}).mount();
+
+new Splide('#clients', {
+  type : 'loop',
+  fixedHeight: '10vh',
+  cover: true,
+  gap: '5vw',
+  focus: 'center',
+  perPage: 3,
+  padding: {
+    right: '4.8vw',
+    left : '5vw',
+  },
+}).mount();
+
